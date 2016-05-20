814412fd36531e68730a46152a81c3cb55e093bc
sergshner <serg.flashtech@gmail.com>
2016-05-21 00:39:40 +0300

    Add mysql root pass parameter

--- s3sync_init.sh
+++ s3sync_init.sh
@@ -3,6 +3,7 @@ BACKUP_BUCKET=myproject-backups
 LOGS_BUCKET=myproject-logs
 BACKUP_WEB_PATH=/var/www/html
 BACKUP_MYSQL_PATH=/var/docker/backups/mysql_backup.sql
+MYSQL_ROOT_PASS={{ mysql_root_password }}
 LOGS_DIRS=(
 	"/var/docker/log/*"
 )
